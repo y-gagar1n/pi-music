89457e9a1d149153c3611c863ad49c8fc65c28f7
y-gagar1n <y.timofeyev@gmail.com>
2013-06-30 09:16:37 -0700

    PEP8 formatting, removed unused imports and trailing spaces

--- lastfm.py
+++ lastfm.py
@@ -4,54 +4,59 @@ import config
 
 API_KEY = config.last_fm['API_KEY']
 API_SECRET = config.last_fm['API_SECRET']
-LASTFM_URL='ws.audioscrobbler.com'
+LASTFM_URL = 'ws.audioscrobbler.com'
 
-class lastfm_controller:	
-	def request(self, query):    
-	    conn = hl.HTTPConnection(LASTFM_URL)
-	 
-	    conn.request('GET', '/2.0/?' + query)
-	    response = conn.getresponse()
-	    return response.read()
-	    conn.close()
 
-	def get_album_info_query(self, artist, album):	    	    
-	    artist=''.join(artist).encode('cp1251')
-	    album=''.join(album).encode('cp1251')
-	 
-	    artist=ul.quote_plus(artist)
-	    album=ul.quote_plus(album)
+class lastfm_controller:
 
-	    data = {"api_key" : API_KEY, 'artist' : artist, 'album' : album, 'autocorrect' : '1', 'method' : 'album.getinfo'}
-	    return ul.unquote(ul.urlencode(data))
+    def request(self, query):
+        conn = hl.HTTPConnection(LASTFM_URL)
 
-	def get_top_albums_query(self, artist):
-	    artist=''.join(artist).encode('cp1251')
-	    artist=ul.quote_plus(artist)
+        conn.request('GET', '/2.0/?' + query)
+        response = conn.getresponse()
+        return response.read()
+        conn.close()
 
-	    data = {"api_key" : API_KEY, 'artist' : artist, 'autocorrect' : '1', 'method' : 'artist.getTopAlbums'}
-	    return ul.unquote(ul.urlencode(data))
+    def get_album_info_query(self, artist, album):
+        artist = ''.join(artist).encode('cp1251')
+        album = ''.join(album).encode('cp1251')
 
-	def get_top_tracks_query(self, artist):
-	    artist=''.join(artist).encode('cp1251')
-	    artist=ul.quote_plus(artist)
+        artist = ul.quote_plus(artist)
+        album = ul.quote_plus(album)
 
-	    data = {"api_key" : API_KEY, 'artist' : artist, 'autocorrect' : '1', 'method' : 'artist.getTopTracks'}
-	    return ul.unquote(ul.urlencode(data))
+        data = {"api_key": API_KEY, 'artist': artist, 'album':
+                album, 'autocorrect': '1', 'method': 'album.getinfo'}
+        return ul.unquote(ul.urlencode(data))
 
-	def getalbuminfo(self, artist, album):		
-		query = self.get_album_info_query(artist, album)		
-		response = self.request(query)		
-		return response
+    def get_top_albums_query(self, artist):
+        artist = ''.join(artist).encode('cp1251')
+        artist = ul.quote_plus(artist)
 
-	def gettopalbums(self, artist):
-		query = self.get_top_albums_query(artist)
-		print query
-		response = self.request(query)		
-		print response
-		return response
+        data = {"api_key": API_KEY, 'artist': artist,
+                'autocorrect': '1', 'method': 'artist.getTopAlbums'}
+        return ul.unquote(ul.urlencode(data))
 
-	def gettoptracks(self, artist):
-		query = self.get_top_tracks_query(artist)
-		response = self.request(query)		
-		return response
+    def get_top_tracks_query(self, artist):
+        artist = ''.join(artist).encode('cp1251')
+        artist = ul.quote_plus(artist)
+
+        data = {"api_key": API_KEY, 'artist': artist,
+                'autocorrect': '1', 'method': 'artist.getTopTracks'}
+        return ul.unquote(ul.urlencode(data))
+
+    def getalbuminfo(self, artist, album):
+        query = self.get_album_info_query(artist, album)
+        response = self.request(query)
+        return response
+
+    def gettopalbums(self, artist):
+        query = self.get_top_albums_query(artist)
+        print query
+        response = self.request(query)
+        print response
+        return response
+
+    def gettoptracks(self, artist):
+        query = self.get_top_tracks_query(artist)
+        response = self.request(query)
+        return response
--- mpd_client.py
+++ mpd_client.py
@@ -1,78 +1,83 @@
 import mpd
-import pprint
-import math
+
 
 class play_stream:
-	def __init__(self):
-		self.client = mpd.MPDClient()
-		self.client.connect("localhost", 6600)	
-		self.clear()
-		self.playlist = {}
 
-	def addplay(self, stream_uri, artist, title, duration):		
-		songid = self.client.addid(stream_uri)
-		self.playlist[songid] = { "id" : songid, "uri" : stream_uri, "artist" : artist, "title" : title, "duration" : duration }
-		self.client.playid(songid)
-		return songid
+    def __init__(self):
+        self.client = mpd.MPDClient()
+        self.client.connect("localhost", 6600)
+        self.clear()
+        self.playlist = {}
 
-	def addplayrange(self, list):		
-		first = True
-		songid = 0
-		for item in list:			
-			if(first):				
-				self.addplay(item["href"], item["artist"], item["title"], item["duration"])
-				first = False
-			else:
-				self.add(item["href"], item["artist"], item["title"], item["duration"])			
+    def addplay(self, stream_uri, artist, title, duration):
+        songid = self.client.addid(stream_uri)
+        self.playlist[songid] = {"id": songid,
+                                 "uri": stream_uri, "artist": artist, "title": title, "duration": duration}
+        self.client.playid(songid)
+        return songid
 
-	def add(self, stream_uri, artist, title, duration):		
-		songid = self.client.addid(stream_uri)
-		self.playlist[songid] = { "id" : songid, "uri" : stream_uri, "artist" : artist, "title" : title, "duration" : duration }
+    def addplayrange(self, list):
+        first = True
+        songid = 0
+        for item in list:
+            if(first):
+                self.addplay(item["href"], item[
+                             "artist"], item["title"], item["duration"])
+                first = False
+            else:
+                self.add(item["href"], item[
+                         "artist"], item["title"], item["duration"])
 
-	def play(self, songid):
-		self.client.playid(songid)
+    def add(self, stream_uri, artist, title, duration):
+        songid = self.client.addid(stream_uri)
+        self.playlist[songid] = {"id": songid, "uri": stream_uri,
+                                 "artist": artist, "title": title, "duration": duration}
 
-	def is_playing(self):
-		state = self.client.status()['state']		
-		return state == "play"
+    def play(self, songid):
+        self.client.playid(songid)
 
-	def clear(self):
-		self.client.clear()
+    def is_playing(self):
+        state = self.client.status()['state']
+        return state == "play"
 
-	def pause(self):
-		state = self.client.status()['state']		
-		pause = 1 if state != 'pause' else 0 		
-		self.client.pause(pause)
+    def clear(self):
+        self.client.clear()
 
-	def previous(self):
-		self.client.prevoius()
+    def pause(self):
+        state = self.client.status()['state']
+        pause = 1 if state != 'pause' else 0
+        self.client.pause(pause)
 
-	def next(self):
-		self.client.next()
+    def previous(self):
+        self.client.prevoius()
 
-	def addvolume(self, vol):		
-		old_volume = int(self.client.status()["volume"])
-		new_volume = old_volume + vol
-		
-		if new_volume < 0:
-			new_volume = 0
-		if new_volume > 100:
-			new_volume = 100
-		try:
-			self.client.setvol(new_volume)
-		except Exception as e:
-			print e
-		
-	def getplaylistinfo(self):
-		playlistinfo = self.client.playlistinfo()		
-		currentsonginfo = self.client.currentsong()		
-		result = []
-		for item in playlistinfo:
-			item_to_add = self.playlist[item["id"]]
-			item_to_add["current"]	= (item_to_add["id"] == currentsonginfo["id"])
-			result.append(item_to_add)
-		return result
+    def next(self):
+        self.client.next()
 
-	def currentsong(self):
-		songid = self.client.currentsong()["id"]		
-		return self.playlist[songid]
+    def addvolume(self, vol):
+        old_volume = int(self.client.status()["volume"])
+        new_volume = old_volume + vol
+
+        if new_volume < 0:
+            new_volume = 0
+        if new_volume > 100:
+            new_volume = 100
+        try:
+            self.client.setvol(new_volume)
+        except Exception as e:
+            print e
+
+    def getplaylistinfo(self):
+        playlistinfo = self.client.playlistinfo()
+        currentsonginfo = self.client.currentsong()
+        result = []
+        for item in playlistinfo:
+            item_to_add = self.playlist[item["id"]]
+            item_to_add["current"] = (item_to_add[
+                                      "id"] == currentsonginfo["id"])
+            result.append(item_to_add)
+        return result
+
+    def currentsong(self):
+        songid = self.client.currentsong()["id"]
+        return self.playlist[songid]
